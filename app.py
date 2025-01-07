3f027e3e512c8d4263b5e9b23908c09453436f37
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-01-07 22:02:02 +0700

    Link Home, About Us, ChatBot, and Finding

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template("home.html")
+
+@app.route("/aboutUs")
+def aboutUs():
+    return render_template("aboutUs.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+@app.route("/finding")
+def finding():
+    return render_template("finding.html")
 
 app.run(debug=True)
