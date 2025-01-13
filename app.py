247daa5bc196a7e7c3ab9b2b7ca6b24d626d31bb
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-01-13 23:08:43 +0700

    Add basic user login and signup

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, redirect, session, url_for
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "top_secret_secret_key"
 
 @app.route("/")
 def home():
@@ -22,6 +24,7 @@ def finding():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
+        username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
 
@@ -30,16 +33,54 @@ def signup():
 
         hashed_password = generate_password_hash(password)
 
-        # store user data in db
-        # TODO: db insertion code here
-        print("insert", email, hashed_password)
+        try:
+            conn = sqlite3.connect("database.db")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                           (username, email, hashed_password))
+            conn.commit()
+            conn.close()
+        except sqlite3.IntegrityError:
+            return render_template("signUp.html", message="Username or Email already exists.")
 
         return redirect("/login")
 
     return render_template("signUp.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # retrieve user
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, username, email, password FROM users WHERE email = ?', (email,))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user and check_password_hash(user[3], password):
+            session["user_id"] = user[0]
+            print(user[0], user[1], "login successfully")
+            return redirect("/user_home")
+        else:
+            return render_template("login.html", message="Invalid username or password.")
+
     return render_template("login.html")
+
+@app.route("/user_home")
+def user_home():
+    if 'user_id' not in session:
+        return redirect('/')
+    user_id = session['user_id']
+
+    return render_template("user_home.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 app.run(debug=True)
--- create_db.py
+++ create_db.py
@@ -7,6 +7,7 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 )
